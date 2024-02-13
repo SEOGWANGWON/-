5922155e03920bfd82195ae15a376d007e68f369
lee-hyunwooo <143372395+lee-hyunwooo@users.noreply.github.com>
2024-02-13 15:10:35 +0900

    Delete spring-server directory

--- spring-server/PenPick/src/main/java/com/penpick/fresh/repository/ProductItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.penpick.fresh.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.penpick.fresh.model.ProductItem;
-
-public interface ProductItemRepository extends JpaRepository<ProductItem, Long>{
-
-}
