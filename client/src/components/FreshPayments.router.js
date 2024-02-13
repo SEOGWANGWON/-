75bc418ca86a1d8721b938e717c7f55612364d4f
lee-hyunwooo <143372395+lee-hyunwooo@users.noreply.github.com>
2024-02-13 15:10:08 +0900

    Delete client directory

--- client/src/components/FreshPayments.router.js
+++
@@ -1,8 +0,0 @@
-// read-only
-const router = require("express").Router();
-
-const controller = require("./FreshPayments.controller");
-
-router.route("/confirm").get(controller.confirmPayment);
-
-module.exports = router;
--- client/src/components/ReservationPaymentRouter.js
+++
@@ -1,8 +0,0 @@
-// read-only
-const router = require('express').Router();
-
-const controller = require('./ReservationController');
-
-router.route('/confirm').get(controller.confirmPayment);
-
-module.exports = router;
